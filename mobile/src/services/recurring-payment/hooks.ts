255384401f43d015cefac52cdd90661e946cd131
rakss17 <bohariambulo17@gmail.com>
2026-09-23 22:14:33 +0800

    feat(mobile): add recurring payment details, update, and delete service

--- mobile/src/services/recurring-payment/hooks.ts
+++ mobile/src/services/recurring-payment/hooks.ts
@@ -2,7 +2,10 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 import {
   createRecurringPaymentApi,
+  deleteRecurringPaymentApi,
+  getRecurringPaymentApi,
   getRecurringPaymentsApi,
+  updateRecurringPaymentApi,
 } from "@/services/recurring-payment/api";
 import { getErrorMessage } from "@/services/get-error-message";
 import { FindAllRecurringPaymentParams } from "./types";
@@ -45,4 +48,74 @@ export const useCreateRecurringPayment = () => {
   const errorMessage = getErrorMessage(error);
 
   return { createRecurringPayment, isPending, errorMessage };
+};
+
+export const useRecurringPayment = (id: string) => {
+  const { data, isPending, error } = useQuery({
+    queryKey: ["recurring-payment", id],
+    queryFn: () => getRecurringPaymentApi(id),
+    enabled: !!id,
+  });
+
+  const errorMessage = getErrorMessage(error);
+
+  return {
+    recurringPayment: data?.data,
+    isPending,
+    errorMessage,
+  };
+};
+
+export const useUpdateRecurringPayment = () => {
+  const queryClient = useQueryClient();
+
+  const {
+    mutate: updateRecurringPayment,
+    isPending,
+    error,
+  } = useMutation({
+    mutationFn: updateRecurringPaymentApi,
+    onSuccess: (_data, { id }) => {
+      queryClient.invalidateQueries({
+        queryKey: ["recurring-payment", id],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["recurring-payments"],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["dashboard-overview"],
+      });
+    },
+  });
+
+  const errorMessage = getErrorMessage(error);
+
+  return { updateRecurringPayment, isPending, errorMessage };
+};
+
+export const useDeleteRecurringPayment = () => {
+  const queryClient = useQueryClient();
+
+  const {
+    mutate: deleteRecurringPayment,
+    isPending,
+    error,
+  } = useMutation({
+    mutationFn: deleteRecurringPaymentApi,
+    onSuccess: (_data, id) => {
+      queryClient.removeQueries({
+        queryKey: ["recurring-payment", id],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["recurring-payments"],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["dashboard-overview"],
+      });
+    },
+  });
+
+  const errorMessage = getErrorMessage(error);
+
+  return { deleteRecurringPayment, isPending, errorMessage };
 };
